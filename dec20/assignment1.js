7687d173c89d5abeb9c7c3fd5232b3886c57a761
Anshul verma <43145798+Anshulverma33@users.noreply.github.com>
2021-12-30 00:26:55 +0530

    Add files via upload

---
+++ dec20/assignment1.js
@@ -0,0 +1,8 @@
+//console/log("hii");
+
+var n=5;
+if(n>10){
+    console.log("Greater");
+}else{
+    console.log("Smaller");
+}
---
+++ dec20/assignment2.js
@@ -0,0 +1,12 @@
+//console.log("hii");
+
+var a=30;
+var b=20;
+var c=50;
+if(a>b && a>c){
+    console.log(a+ " is Highest Number");
+}else if(b>a && b>c){
+    console.log(b+ " is Highest Number");
+}else{
+    console.log(c+ " is Highest Number");
+}
