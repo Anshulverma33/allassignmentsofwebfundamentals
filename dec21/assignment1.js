e2eabe0e43f6951fde0e4d5a1f2eb769fdf4d0db
Anshul verma <43145798+Anshulverma33@users.noreply.github.com>
2021-12-29 22:57:03 +0530

    Add files via upload

---
+++ dec21/assignment1.js
@@ -0,0 +1,12 @@
+var a = prompt("Enter number 1");
+var b = prompt("Enter number 2");
+var c = prompt("Enter number 3");
+
+if (a <= b && a <= c)
+        alert( a + " is the smallest");
+ 
+    else if (b <= a && b <= c)
+        alert( b + " is the smallest");
+ 
+    else
+        alert( c + " is the smallest");
---
+++ dec21/assignment2.js
@@ -0,0 +1,9 @@
+var x = prompt("Enter a Vowel charcter");
+
+if (x == 'a' || x == 'e' || x == 'i' ||
+    x == 'o' || x == 'u' || x == 'A' ||
+    x == 'E' || x == 'I' || x == 'O' || x == 'U')
+        alert(x + " is Vowel");
+    else
+        alert(x + " is Consonant" );
+
